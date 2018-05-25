2bac47634fdfa47df3138ecc71b76fdbb7edc4c0
Anwarus <Anwarus1289@gmail.com>
2018-05-25 20:36:27 +0200

    change static functions to not use arrow

--- js/MapCreator.js
+++ js/MapCreator.js
@@ -3,11 +3,11 @@ export class MapCreator {
 
     }
 
-    static generate = (width, height, floors, walls) => {
+    static generate(width, height, floors, walls) {
         let startPoint = this.getRandomPoint(width, height);
     }
 
-    static getRandomPoint = (xRange, yRange) => {
+    static getRandomPoint(xRange, yRange) {
         return {
             x: Math.random() % width,
             y: Math.random() % height
