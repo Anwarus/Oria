18f8b1fd983efb81f88e0ae6540a09b3ca4ee566
Anwarus <Anwarus1289@gmail.com>
2018-05-28 22:37:04 +0200

    intrduced map length in MapCreator

--- js/MapCreator.js
+++ js/MapCreator.js
@@ -3,8 +3,12 @@ export class MapCreator {
 
     }
 
-    static generate(width, height, floors, walls) {
+    static generate(width, height, floors, walls, length) {
         let startPoint = this.getRandomPoint(width, height);
+
+        for(let i=0; i<length; i++) {
+            
+        }
     }
 
     static getRandomPoint(xRange, yRange) {
