ff10ab52b2769070be461e225472dd43d8360e62
Anwarus <Anwarus1289@gmail.com>
2018-06-22 19:56:21 +0200

    get possible direction of next field in MapGenerator

--- js/MapGenerator.js
+++ js/MapGenerator.js
@@ -1,13 +1,23 @@
+const Type = {
+    EMPTY: 0,
+    FLOOR: 1,
+    WALL: 2
+}
+
 export class MapCreator {
     constructor({} = {}) {
 
     }
 
-    static generate(width, height, floors, walls, length) {
+    static generate(width, height, length) {
+        let map = new Array(width)
+            .fill(new Array(height)
+                .fill(Type.EMPTY));
+
         let startPoint = this.getRandomPoint(width, height);
 
         for(let i=0; i<length; i++) {
-            
+            let possibleDirections = this.getPossibleDirections(map, startPoint);
         }
     }
 
@@ -16,5 +26,47 @@ export class MapCreator {
             x: Math.random() % width,
             y: Math.random() % height
         };
+    }
+
+    static getPossibleDirections(map, point) {
+        let possibleDirections = [];
+
+        let direction = point.x - 1;
+        if(direction >= 0) {
+            if(map[direction, point.y] === 0)
+                possibleDirections.push({
+                    x: direction,
+                    y: point.y
+                });
+        }
+
+        direction = point.x + 1;
+        if(direction < map.length) {
+            if(map[direction, point.y] === 0)
+                possibleDirections.push({
+                    x: direction,
+                    y: point.y
+                });
+        }
+
+        direction = point.y - 1;
+        if(direction >= 0) {
+            if(map[point.x, direction] === 0)
+                possibleDirections.push({
+                    x: point.x,
+                    y: direction
+                });
+        }
+
+        direction = point.y + 1;
+        if(direction < map[0].length) {
+            if(map[point.x, direction] === 0)
+                possibleDirections.push({
+                    x: point,y,
+                    y: direction
+                });
+        }
+
+        return possibleDirections;
     }
 }
